77bb9975f11d987355e8207b1e5d89e2848ae607
Chuck King <cwkingjr@gmail.com>
2025-08-09 06:34:20 -0500

    wip trying pyqt

--- src/csv_consolidate/main.py
+++ src/csv_consolidate/main.py
@@ -1,32 +1,38 @@
-from nicegui import app, ui
-from webview import FOLDER_DIALOG
-
-app.native.window_args["resizable"] = False
-# app.native.start_args["debug"] = True
-app.native.settings["ALLOW_DOWNLOADS"] = True
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog
 
 
-@ui.page("/")
-def main_page():
-    async def pick_directory():
-        folder = await app.native.main_window.create_file_dialog(
-            allow_multiple=False,
-            dialog_type=FOLDER_DIALOG,
-        )
-        if folder:
-            ui.notify(f"Selected directory: {folder}")
-        else:
-            ui.notify("No directory selected.")
+class FileDialogExample(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-    ui.label("app running in native mode")
-    ui.button("enlarge", on_click=lambda: app.native.main_window.resize(1000, 700))
-    ui.button("Pick Directory", on_click=pick_directory)
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("QFileDialog Example - Select Directory")
+        self.setGeometry(100, 100, 400, 300)
+
+        self.button = QPushButton("Select Directory", self)
+        self.button.clicked.connect(self.selectDirectoryDialog)
+        self.button.setGeometry(150, 150, 150, 30)
+
+    def selectDirectoryDialog(self):
+        file_dialog = QFileDialog(self)
+        file_dialog.setWindowTitle("Select Directory")
+        file_dialog.setFileMode(QFileDialog.FileMode.Directory)
+        file_dialog.setViewMode(QFileDialog.ViewMode.List)
+
+        if file_dialog.exec():
+            selected_directory = file_dialog.selectedFiles()[0]
+            print("Selected Directory:", selected_directory)
 
 
-ui.run(
-    title="CSV Consolidate",
-    native=True,
-    window_size=(400, 300),
-    fullscreen=False,
-    reload=False,
-)
+def main():
+    app = QApplication(sys.argv)
+    window = FileDialogExample()
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
